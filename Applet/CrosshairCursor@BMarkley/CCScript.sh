d1dcfaf68c9b6a3badbbaf8af134326b4689925f
Brian Markley PENG RSE <BMarkley@ualberta.ca>
2026-04-09 03:10:52 -0600

    Delete Applet/CrosshairCursor@BMarkley/CCScript.sh

--- Applet/CrosshairCursor@BMarkley/CCScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if pgrep -x "CrosshairCursor" > /dev/null
-then
-    pkill -x CrosshairCursor
-    sleep 0.2
-else
-    ~/.local/share/cinnamon/applets/CrosshairCursor@BMarkley/CrosshairCursor/CrosshairCursor -fg lightgrey -bg black &
-    sleep 0.2
-fi
